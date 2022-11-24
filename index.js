6d62f1190fc33d5e4e7c42380633d19b8d539c60
راكينول إسلام <you@example.com>
2022-11-24 19:09:40 +0600

    server ready

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('node server in running')
+})
+app.listen(port, () =>{
+    console.log(`node server in running on port${port}`);
+})
