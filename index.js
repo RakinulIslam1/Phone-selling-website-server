9e1aa045d6c6b123ed549da49544d2809a051a0f
راكينول إسلام <you@example.com>
2022-12-02 20:46:07 +0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 
 const app = express();
@@ -7,6 +8,25 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.avq6v53.mongodb.net/?retryWrites=true&w=majority`;
+  console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) =>{
     res.send('node server in running')
